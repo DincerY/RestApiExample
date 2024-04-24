9cc4381acf1de6b3df7167c5f16e6ad7583d158f
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-04-24 16:10:39 +0300

    first commit

---
+++ app/jwtAuthentication.go
@@ -0,0 +1,45 @@
+package app
+
+import (
+	u "RestApiExample/utils"
+	"net/http"
+	"strings"
+)
+
+var JwtAuthentication = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		notAuth := []string{"/api/user/new", "/api/user/login"}
+		requestPath := r.URL.Path
+
+		for _, value := range notAuth {
+			if value == requestPath {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		response := make(map[string]interface{})
+		tokenHeader := r.Header.Get("Authorization")
+
+		if tokenHeader == "" {
+			response = u.Message(false, "Token gönderilmelidir")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		splitted := strings.Split(tokenHeader, " ")
+		if len(splitted) != 2 {
+			//Bu dönüş değerini böyle değilde bir hazır format olarak yapmaya çalışalım
+			response := u.Message(false, "Hatalı yada geçersiz token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+	})
+
+}
